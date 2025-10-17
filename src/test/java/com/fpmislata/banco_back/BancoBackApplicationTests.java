9835526d09f34111124f5bf2c11c760309345d58
Jaime Gómez <jaimejgc2018@gmail.com>
2025-10-17 13:24:53 +0200

    First commit

---
+++ src/test/java/com/fpmislata/banco_back/BancoBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fpmislata.banco_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BancoBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
