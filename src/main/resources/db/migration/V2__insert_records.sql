0436a2b2c58ad96666180375b0315ecc2e618c6f
ZOE ALCAÑIZ SOLER <10745890@cip.fpmislata.com>
2026-02-09 10:25:03 +0100

    añadir usuario tienda

--- src/main/resources/db/migration/V2__insert_records.sql
+++ src/main/resources/db/migration/V2__insert_records.sql
@@ -1,8 +1,10 @@
 INSERT INTO client (dni, user_name, password, name, surname1, surname2, api_token)
 VALUES
-('12345678A', 'jlopez', 'pass123', 'Juan', 'López', 'García', 'token123'),
-('87654321B', 'mgarcia', 'pass456', 'María', 'García', 'Pérez', 'token456'),
-('11223344C', 'aperez', 'pass789', 'Ana', 'Pérez', NULL, 'token789');
+('12345678A', 'jlopez', '123', 'Juan', 'López', 'García', 'token123'),
+('87654321B', 'mgarcia', '123', 'María', 'García', 'Pérez', 'token456'),
+('11223344C', 'aperez', '123', 'Ana', 'Pérez', NULL, 'token789');
+('11111111Z', 'retoquesexpress', '123', 'Retoques', 'Express', NULL, 'token888');
+
 
 INSERT INTO account (iban, balance, id_client)
 VALUES
@@ -10,6 +12,9 @@ VALUES
 ('ES7921000813610123456789', 250.00, '12345678A'),
 ('ES6621000418450200059999', 9800.75, '87654321B'),
 ('ES5621014813710123449772', 1.00, '11223344C');
+('ES1111111111111111111111', 1000.00, '11111111Z');
+
+
 
 
 INSERT INTO accountmovement
@@ -25,4 +30,6 @@ VALUES
 (1, '4111111111111111', '2027-12-31', 123, 'Juan López', 'ES9121000418450200051332'),
 (2, '5500000000000004', '2028-06-30', 456, 'María García', 'ES6621000418450200059999'),
 (3, '340000000000009', '2026-09-30', 789, 'Ana Pérez', 'ES5621014813710123449772');
+(4, '11111111111111', '2027-09-30', 123, 'retoques express', 'ES1111111111111111111111');
+
 
